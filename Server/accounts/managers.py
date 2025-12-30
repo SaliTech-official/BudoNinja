e65c7538fa86351b43bb5a3339d10b77e96ddf60
mahan-rezaei <mahanrezaei859@gmail.com>
2025-12-30 23:27:20 +0330

    add command for adding province/cities datas and add theme in db./

--- Server/accounts/managers.py
+++ Server/accounts/managers.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, full_name, phone_number, national_code, password=None, **extra_fields):
+    def create_user(self, full_name, phone_number, password=None, **extra_fields):
         if not full_name:
             raise ValueError('full name must be set')
 
@@ -12,13 +12,13 @@ class UserManager(BaseUserManager):
         
         if not password:
             raise ValueError('password is required.')
-        user = self.model(full_name=full_name, phone_number=phone_number, national_code=national_code, **extra_fields)
+        user = self.model(full_name=full_name, phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, full_name, phone_number, national_code, password=None, **extra_fields):
-        user = self.create_user(full_name, phone_number, password, national_code **extra_fields)
+    def create_superuser(self, full_name, phone_number, password=None, **extra_fields):
+        user = self.create_user(full_name, phone_number, password, **extra_fields)
         user.is_admin = True
         user.is_superuser = True
         user.is_verified = True
--- Server/data/admin.py
+++ Server/data/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Province, City
 
-# Register your models here.
+@admin.register(Province)
+class ProvineAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ("naem",)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'province')
+    list_filter = ('province',)
+    search_fields = ('name',)
