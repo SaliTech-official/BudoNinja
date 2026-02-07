626da051de2c6996d4aae32e1cf4beb59cbbe0ff
Salar <SaliTech.2008@gmail.com>
2026-02-07 23:15:54 +0330

    HomePage Update

--- Client/budoninja_client/src/pages/HomePage.tsx
+++ Client/budoninja_client/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { HeroSection } from '../components/Public/Home/HeroSection'
+import { AboutStatsSection } from '../components/Public/Home/AboutStatsSection'
 
 export default function HomePage() {
   return (
     <div>
         <HeroSection />
+        <AboutStatsSection />
     </div>
   )
 }
