b111fd3aa51b5694618bbafbd48b9e014a91d574
Salar <SaliTech.2008@gmail.com>
2026-02-26 16:48:15 +0330

    Register - Step 3 Add

--- Client/budoninja_client/src/components/auth/Step3Form.tsx
+++ Client/budoninja_client/src/components/auth/Step3Form.tsx
@@ -1,53 +1,58 @@
-import { Input } from "../UI/Input";
-import { Button } from "../UI/Button";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Button } from '../UI/Button';
+import { Link } from 'react-router-dom';
+import { OtpInput } from '../UI/OtpInput';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 interface Step3FormProps {
   onBack: () => void;
-  mobileNumber: string; // شماره موبایل از مرحله قبل میاد
+  mobileNumber: string; 
 }
 
 export function Step3Form({ onBack, mobileNumber }: Step3FormProps) {
-  // بعداً اینجا منطق تایمر رو اضافه می‌کنیم
-  // const [timer, setTimer] = useState(120);
+  const handleOtpComplete = (otp: string) => {
+    console.log("کد وارد شده:", otp);
+  };
+
+  const timer = "01:59";
 
   return (
-    <form className="space-y-6 text-center">
-      
-      {/* متن راهنما */}
-      <div className="mb-8">
-        <h3 className="text-xl font-bold text-neutral-800">تایید شماره موبایل</h3>
-        <p className="mt-2 text-sm text-neutral-600">
-          کد تایید ۶ رقمی به شماره 
-          <span className="font-bold mx-1">{mobileNumber}</span>
-          ارسال شد.
-        </p>
-      </div>
-      
-      {/* فیلد OTP */}
-      <Input
-        id="otp-code"
-        placeholder="- - - - - -"
-        className="text-center text-2xl tracking-[1rem]" // 👈 برای فاصله بین اعداد
-        maxLength={6}
-      />
-      
-      {/* تایمر و ارسال مجدد */}
-      <div className="text-sm text-neutral-500">
-        <span>01:59</span>
-        <Button variant="link" className="mx-2">
-          ارسال مجدد کد
-        </Button>
+    <form className="text-center flex flex-col gap-10">
+      <div className='flex flex-col gap-6'>
+        <div className='flex flex-col gap-8'>
+          <p className="text-sm text-neutral-600">
+            کد تایید 5 رقمی به شماره 
+            <span className="font-bold mx-1 dir-ltr inline-block">{mobileNumber}</span>
+            ارسال شد.
+          </p>
+          <OtpInput length={5} onComplete={handleOtpComplete} />
+        </div>
+        
+        
+        <div className="text-sm text-neutral-500">
+          <span>{timer}</span>
+          <span className="mx-2">تا ارسال مجدد کد</span>
+          <Button variant="link" type="button" className="text-sm hidden">
+            ارسال مجدد
+          </Button>
+        </div>
       </div>
 
-      {/* دکمه‌های ناوبری */}
-      <div className="flex gap-4 pt-4">
-        <Button onClick={onBack} variant="outline" className="w-full">
-          مرحله قبل
-        </Button>
-        <Button type="submit" className="w-full">
-          تایید و ساخت حساب
-        </Button>
+      <div className='flex flex-col gap-6'>
+        <div className="flex flex-col-reverse sm:flex-row gap-4 pt-4">
+          <Button onClick={onBack} variant="outline" className="w-full gap-2 hover:bg-primary-50 hover:text-primary-700" type="button">
+            <ChevronRight />
+            <span>ویرایش شماره</span>
+          </Button>
+          <Button type="submit" className="w-full gap-2">
+            <span>تایید و ورود به پنل</span>
+            <ChevronLeft />
+          </Button>
+        </div>
+        <div className='flex gap-1 text-sm justify-center'>
+          <span className='font-400 text-neutral-500'>حساب دارید؟</span>
+          <Link className='font-semibold text-primary-600' to="/login">ورود</Link>
+        </div>
       </div>
 
     </form>
