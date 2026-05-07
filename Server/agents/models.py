fb7fa5c739a61af39410ac168dc5e852c6380c25
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-07 15:02:00 +0330

    add teachers model./

--- Server/agents/models.py
+++ Server/agents/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from data.models import Province
 
-# Create your models here.
+
+class Teacher(models.Model):
+    ROLE_CHOICES = (
+        ('senior', 'Senior'),
+        ('normal', 'Normal')
+    )
+
+    full_name = models.CharField(max_length=258)
+    level = models.CharField(max_length=258)
+    branch = models.CharField(max_length=128)
+    role = models.CharField(max_length=68, choices=ROLE_CHOICES, default='normal')
+    province = models.ForeignKey(Province, on_delete=models.SET_NULL, null=True, blank=True, related_name='Teachers')
--- Server/ninja/settings.py
+++ Server/ninja/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
     'data.apps.DataConfig',
     'accounts.apps.AccountsConfig',
     'news.apps.NewsConfig',
-    'agents.apps.AgentsConfig'
+    'agents.apps.AgentsConfig',
     
 
     # THIRD PARTIES
