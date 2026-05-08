3d0f640cef8011c70f222a0ba71ec757e5dcb8ce
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-08 22:29:52 +0330

    add chalenge model./

--- Server/chalenge/models.py
+++ Server/chalenge/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Chalenge(models.Model):
+    TYPE_CHOICES = (
+        ('solo', 'انفرادی'),
+        ('team', 'تیمی'),
+    )
+
+    GENDER_CHOICES = (
+        ('male', 'اقایان'),
+        ('female', 'بانوان'),
+        ('ob', 'کونی'),
+    )
+
+    title = models.CharField(max_length=128)
+    explain = models.TextField()
+    place = models.CharField(max_length=128)
+    chalenge_type = models.CharField(choices=TYPE_CHOICES, default='solo')
+    gender = models.CharField(choices=GENDER_CHOICES)
+
+    is_open = models.BooleanField(default=True)
+
+    date = models.DateField()
+    deadline = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} - {self.place}"
