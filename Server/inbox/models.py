41adb048e5c6925507b9d47429742614818782b3
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-10 22:20:46 +0330

    add Ticket model./

--- Server/inbox/models.py
+++ Server/inbox/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    full_name = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=11)
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.full_name} - {self.title}"
