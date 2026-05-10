364a4872521514c7f84aaa03034b3c0b1a60378b
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-10 22:42:18 +0330

    make some change in ticket model and register it to admin./

--- Server/inbox/admin.py
+++ Server/inbox/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from inbox.models import Ticket
 
-# Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'title', 'phone_number', 'created_at', 'status')
+    list_filter = ('status',)
+    search_fields = ('phone_number',)
--- Server/inbox/models.py
+++ Server/inbox/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django_jalali.db import models as jmodels
+from django.utils import timezone
 
 
 class Ticket(models.Model):
+    STATUS_CHOICE = (
+        ('open', 'Open'),
+        ('closed', 'Closed'),
+    )
+
     full_name = models.CharField(max_length=128)
     phone_number = models.CharField(max_length=11)
     title = models.CharField(max_length=128)
     content = models.TextField()
+
+    status = models.CharField(max_length=68, choices=STATUS_CHOICE, default='open')
+
+    created_at = jmodels.jDateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.full_name} - {self.title}"
