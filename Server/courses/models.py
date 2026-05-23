a47d416cfeabaede9cfc5cfe9dc6d7fbff8e0b28
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-23 22:15:08 +0330

    add course model./

--- Server/courses/models.py
+++ Server/courses/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from agents.models import Teacher
 
-# Create your models here.
+
+class course(models.Model):
+    title = models.CharField(max_length=128)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name="courses")
+    duration = models.DurationField()
+    capasity = models.SmallIntegerField(default=0)
