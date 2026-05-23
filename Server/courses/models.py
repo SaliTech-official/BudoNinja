39d94e563db2b266427166a126dbfd3a793315d1
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-23 22:31:33 +0330

    change some fields in course model|register model in admin./

--- Server/courses/models.py
+++ Server/courses/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from agents.models import Teacher
+from django_jalali.db import models as jmodels
 
 
-class course(models.Model):
+
+class Course(models.Model):
     title = models.CharField(max_length=128)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name="courses")
     duration = models.DurationField()
     capasity = models.SmallIntegerField(default=0)
+    price = models.DecimalField(max_digits=13, decimal_places=0)
+
+    created_at = jmodels.jDateTimeField(auto_now_add=True)
+    updated_at = jmodels.jDateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
