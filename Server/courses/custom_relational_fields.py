4925bee7fb116f2b8c2a62fc7ad56f29aa960264
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-24 22:27:34 +0330

    add custom field for category in course serializer./

--- Server/courses/custom_relational_fields.py
+++ Server/courses/custom_relational_fields.py
@@ -4,3 +4,8 @@ from rest_framework import serializers
 class CustomTeacherRelationalField(serializers.RelatedField):
     def to_representation(self, value):
         return value.full_name
+
+
+class CustomCategoryRelationalField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.name
