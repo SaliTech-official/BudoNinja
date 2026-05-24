defde02659bf2da450858fa68c33f1dea4d00453
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-24 22:24:25 +0330

    add category for course model./

--- Server/courses/models.py
+++ Server/courses/models.py
@@ -3,14 +3,21 @@ from agents.models import Teacher
 from django_jalali.db import models as jmodels
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=68)
+
+    def __str__(self):
+        return self.name
 
 class Course(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='courses', null=True, blank=True)
     title = models.CharField(max_length=128)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name="courses")
     duration = models.DurationField()
     capasity = models.SmallIntegerField(default=0)
     price = models.DecimalField(max_digits=13, decimal_places=0)
     participants = models.SmallIntegerField(default=0)
+
 
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     updated_at = jmodels.jDateTimeField(auto_now=True)
