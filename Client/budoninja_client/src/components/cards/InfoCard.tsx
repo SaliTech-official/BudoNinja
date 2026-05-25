d22bdf39a6eb1ecbf68468e73b413f785b95f23f
Salar <SaliTech.2008@gmail.com>
2026-05-25 22:59:19 +0330

    Info Card Style Update

--- Client/budoninja_client/src/components/cards/InfoCard.tsx
+++ Client/budoninja_client/src/components/cards/InfoCard.tsx
@@ -11,15 +11,14 @@ interface InfoCardProps {
 export function InfoCard({ IconComponent, title, text, className }: InfoCardProps) {
   return (
     <div className={cn(
-      "flex gap-3 items-center rounded-xl bg-neutral-100 p-4 text-center border border-neutral-200",
+      "flex flex-col gap-4 items-center rounded-xl bg-primary-50 p-6 text-center border border-primary-100",
       className
     )}>
       <IconComponent size={24} className="text-primary-600"/>
-      <div className='flex flex-col gap-1'>
-        <h3 className="text-xs font-semibold text-neutral-500 w-fit">{title}</h3>
-        <p className="text-base text-neutral-900 font-semibold">{text}</p>
+      <div className='flex flex-col gap-2'>
+        <h3 className="text-xl font-bold text-neutral-900 w-full">{title}</h3>
+        <p className="text-base text-neutral-700 font-semibold">{text}</p>
       </div>
-      
     </div>
   );
 }
