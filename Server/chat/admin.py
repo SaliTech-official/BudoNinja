01fe6f4d4893b6c9406a9bdb6c69fb2a91561a22
mahan-rezaei <mahanrezaei859@gmail.com>
2026-05-28 11:35:03 +0330

    register chat models in admin./

--- Server/chat/admin.py
+++ Server/chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Conversation, ConversationParticipant, Message
 
-# Register your models here.
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'updated_at', 'last_message')
+
+
+@admin.register(Conversation)
+class ConversationParticipantAdmin(admin.ModelAdmin):
+    list_display = ('conversation', 'user', 'joined_at')
+
+
+@admin.register(Conversation)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('conversation', 'sender', 'created_at')
