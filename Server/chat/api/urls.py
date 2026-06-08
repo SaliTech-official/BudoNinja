ad9a5dbff17cf9a832094a02cfb58db0b6488606
mahan-rezaei <mahanrezaei859@gmail.com>
2026-06-08 20:29:18 +0330

    add endpoint path./

--- Server/chat/api/urls.py
+++ Server/chat/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create_conversation/', views.CreateConversationView.as_view()),
     path('conversation_list/', views.ConversationListView.as_view()),
     path('create_message/', views.CreateMessageView.as_view()),
+    path('forwarde_message/', views.ForwardMessageView.as_view()),
     path('conversation_messages/', views.ConversationMessgesView.as_view()),
 ]
