49677dc16fa4d160454d95f04f025299e4aeba67
mahan-rezaei <mahanrezaei859@gmail.com>
2026-06-15 00:31:12 +0330

    add belt model./

--- Server/certificates/models.py
+++ Server/certificates/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Belt(models.Model):
+    name = models.CharField(max_length=258)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class BeltCertificate(models.Model):
+    pass
