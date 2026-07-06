66aeb81258741c00d04844e1981d7b279bbccbab
mahan-rezaei <mahanrezaei859@gmail.com>
2026-07-06 14:07:43 +0330

    add fields to update profile exclude./

--- Server/accounts/api/serializers.py
+++ Server/accounts/api/serializers.py
@@ -73,6 +73,9 @@ class UserProfileUpdateSerializer(serializers.ModelSerializer):
             "user",
             "created_at",
             "updated_at",
+            "technical_workshops",
+            "jurisprudence",
+            "chalenge_participated",
         )
 
 
