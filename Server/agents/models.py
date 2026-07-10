47cca8d5281f7fbff2f7d824e9e39a26efd21504
mahan-rezaei <mahanrezaei859@gmail.com>
2026-07-10 12:52:36 +0330

    add fileds to Teacher model./

--- Server/agents/models.py
+++ Server/agents/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from data.models import Province
+from data.models import Province, City
 
 
 class Teacher(models.Model):
@@ -8,11 +8,14 @@ class Teacher(models.Model):
         ('normal', 'Normal')
     )
 
+    phone_number = models.CharField(max_length=11)
+    image = models.ImageField(upload_to="agents_avatar")
     full_name = models.CharField(max_length=258)
     level = models.CharField(max_length=258)
     branch = models.CharField(max_length=128)
     role = models.CharField(max_length=68, choices=ROLE_CHOICES, default='normal')
     province = models.ForeignKey(Province, on_delete=models.SET_NULL, null=True, blank=True, related_name='Teachers')
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True, related_name='Teachers')
 
     def __str__(self):
         return f"{self.full_name}"
